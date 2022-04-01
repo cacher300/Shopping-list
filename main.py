1abda2adeca4db8b39145e20c5755b0f40d5bb76
cacher300 <77995433+cacher300@users.noreply.github.com>
2022-04-01 09:59:07 -0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,7 @@
+def Name():
+    userinput = input("What is your name.\nWe are checking if you are gay\nEnter here: ")
+    if userinput == "teddy" or userinput == "Teddy":
+        print("You are gay asf")
+    else:
+        print("You are straight asf")
+Name()
